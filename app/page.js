12d4b7bc69806987e3df194c1e5b3b4f84b459c9
kaisafestus <kaisafestus11@gmail.com>
2026-04-09 08:13:22 +0300

    Colours update

--- app/page.js
+++ app/page.js
@@ -62,11 +62,9 @@ export default function Home() {
         <div className="navbar-container">
           <a href="#" className="navbar-brand">
             <div className="navbar-logo-holder">
-              <img src="/safaricom.png" alt="Safaricom" className="navbar-logo-img" />
+              <img src="./app/safaricom.png" alt="Safaricom" className="navbar-logo-img" />
             </div>
             <div>
-              <div className="navbar-brand-text" style={{ color: '#0ac800f0' }}>Safaricom</div>
-              <div className="navbar-brand-sub" style={{ color: '#060c0371' }}>Fuliza</div>
             </div>
           </a>
           <div className="navbar-menu">
